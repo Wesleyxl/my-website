a44946bdd6128c8a79c6f14f122133670cda679b
wesleyxl <wesley.alvesxl@gmail.com>
2020-09-13 22:13:10 -0300

    started project

---
+++ src/pages/Home/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Container } from './style';
+
+const Home = () => (
+
+    <Container>
+        <h1>Welcome</h1>
+        <h1>to</h1>
+        <h1>React App</h1>
+    </Container>
+
+);
+
+export default Home;
